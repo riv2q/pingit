5cae8938820b491464cfc777b1988a4f551e7c9a
Krystian Różycki <rozycki.krystian@gmail.com>
2022-04-01 13:17:53 +0200

    black on cli

--- pingit/cli.py
+++ pingit/cli.py
@@ -10,20 +10,21 @@ FlaskCLI(app)
 
 
 @app.cli.command()
-@click.option('--host', default='0.0.0.0')
-@click.option('--port', default=5000)
+@click.option("--host", default="0.0.0.0")
+@click.option("--port", default=5000)
 def start_dev_server(host, port):
     """Start development server."""
     click.echo(
         "Starting development server on host={host} port={port}".format(
-            host=host, port=port)
+            host=host, port=port
+        )
     )
     app.run(host=host, port=port, debug=True)
 
 
 @app.cli.command()
-@click.option('--host', default='0.0.0.0')
-@click.option('--port', default=5000)
+@click.option("--host", default="0.0.0.0")
+@click.option("--port", default=5000)
 def start_server(host, port):
     """Start server."""
     click.echo(
@@ -32,7 +33,8 @@ def start_server(host, port):
     )
     command = (
         "gunicorn --bind {host}:{port} --pid gunicorn.pid app:app".format(
-            host=host, port=port)
+            host=host, port=port
+        )
     )
     os.popen(command)
 
